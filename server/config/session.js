2c0e583802fcc9aa7ac1b662435297ff124b528a
poby123 <youngsm12@naver.com>
2020-10-04 20:43:18 +0900

    update config files for heroku deploy

--- server/config/session.js
+++ server/config/session.js
@@ -4,8 +4,8 @@ const MySQLStore = require("express-mysql-session")(session);
 const authdb = require("./database.js");
 
 module.exports = {
-	//secret: process.env.SESSION_SECRET,
-	secret: "session secrets",
+	secret: process.env.SESSION_SECRET,
+	// secret: "session secrets",
 	resave: false,
 	saveUninitialized: false,
 	store: new MySQLStore(authdb),
