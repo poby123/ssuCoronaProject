bfd0b11b2b6f0f0c632442d1c83ab42ea8fef99c
poby123 <youngsm12@naver.com>
2020-11-27 14:03:03 +0900

    auth bcrypt add

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const session = require("express-session");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
 const dbConfig = require("../config/database.js");
@@ -15,20 +16,35 @@ router.post("/signin", function (req, res) {
     if (req.body.id && req.body.password) {
         const id = req.body.id;
         const pw = req.body.password;
-        connection.query("SELECT * FROM tbladmin where id=? and password=?", [id, pw], function (err, results) {
+        connection.query("SELECT password FROM tbladmin where id=?", [id], function (err, results) {
             if (err) {
                 console.log(err);
-            } else if (results.length == 1) {
-                myDate.deletePrevDays();
-                myDate.checkToday();
+                res.json({ flag: false });
+            } else if (results.length > 0) {
+                bcrypt.compare(pw, results[0].password, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                        console.log("error at compare not found");
+                        res.json({ flag: false });
+                    } else {
+                        console.log(result);
+                        if (result) {
+                            myDate.deletePrevDays();
+                            myDate.checkToday();
 
-                req.session.id = results[0].id;
-                req.session.auth = true;
+                            req.session.id = id;
+                            req.session.auth = true;
 
-                //session saving.
-                req.session.save(function () {
-                    console.log("login Success!, id: ", results[0].id);
-                    res.json({ flag: true });
+                            //session saving.
+                            req.session.save(function () {
+                                console.log("login Success!, id: ", id);
+                                res.json({ flag: true });
+                            });
+                        } else {
+                            console.log("password is wrong");
+                            res.json({ flag: false });
+                        }
+                    }
                 });
             } else {
                 console.log("not found");
